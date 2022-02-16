aee0712ff7ab4e1866524d85f496a7a86192ed67
nachomirsol <nachomirsol@gmail.com>
2022-02-16 12:59:42 +0100

    card component layout

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,3 +1,24 @@
+/** Assets */
+import { ArrowIcon } from 'assets/icons/arrow';
+/** Styles */
+import './styles/card.scss';
+
 export const Card = () => {
-	return <div className='card'>Card component</div>;
+	return (
+		<div className='card'>
+			<div className='card__image'>Img</div>
+			<div className='card__content'>
+				<div className='card__content-text'>
+					<h5>Storm</h5>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+						malesuada viverra m...
+					</p>
+				</div>
+				<button>
+					Read more <ArrowIcon />
+				</button>
+			</div>
+		</div>
+	);
 };
