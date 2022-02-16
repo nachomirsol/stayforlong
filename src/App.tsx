55ee8e8df1544d4214ba5ce7ee57fa63b1e31d69
nachomirsol <nachomirsol@gmail.com>
2022-02-16 14:25:41 +0100

    implement search layout and card list style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Card } from 'components/card';
-
+import { Search } from 'components/search';
+/** Styles */
+import './app.scss';
 export const App = () => {
 	return (
-		<div>
-			<Card />
+		<div className='app'>
+			<Search width={'50%'} onChange={() => ({})} onClick={() => ({})} />
+			<div className='cardList'>
+				{[1, 2, 3, 4, 5, 6].map(() => {
+					return <Card />;
+				})}
+			</div>
 		</div>
 	);
 };
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,3 +1,23 @@
-export const Search = () => {
-	return <div className='search'>Search component</div>;
+/** Assets */
+import { SearchIcon } from 'assets/icons/search';
+/** Types */
+import { SearchTypes } from './types';
+/** Styles */
+import './styles/search.scss';
+
+export const Search = ({ width = '100%', onChange, onClick }: SearchTypes) => {
+	return (
+		<div className='search' style={{ width }}>
+			<input
+				aria-label='Search'
+				name='query'
+				placeholder='Search'
+				type='search'
+				onChange={onChange}
+			/>
+			<button onClick={onClick}>
+				<SearchIcon />
+			</button>
+		</div>
+	);
 };
