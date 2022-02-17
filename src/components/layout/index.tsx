4a2302e2aca836eae1511a5730684171d4cd0b20
nachomirsol <nachomirsol@gmail.com>
2022-02-17 10:18:52 +0100

    remove layout component

--- src/components/layout/index.tsx
+++
@@ -1,6 +0,0 @@
-/** Types */
-import { LayoutTypes } from './types';
-
-export const Layout = ({ children }: LayoutTypes) => {
-	return <div className='layout'>{children}</div>;
-};
--- src/components/layout/types/index.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export type LayoutTypes = {
-	children: React.ReactChildren
-}
