77a250ef6ccf1f9653e42fd94b9ab93cfa7c703e
nachomirsol <nachomirsol@gmail.com>
2022-02-17 19:56:08 +0100

    fix test

---
+++ src/hooks/tests/useCharacters.test.tsx
@@ -0,0 +1,32 @@
+import { waitFor } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import { useCharacters } from '../useCharacters';
+
+let storeRedux = {
+	loading: false,
+	error: false,
+	characters: [
+		{ id: '1', name: 'spiderman' },
+		{ id: '2', name: 'iron man' },
+		{ id: '3', name: 'abominable' },
+	],
+};
+const mockDispatch = jest.fn();
+const mockSelector = jest.fn().mockImplementation(() => storeRedux);
+jest.mock('react-redux', () => ({
+	useDispatch: () => mockDispatch,
+	useSelector: () => mockSelector(),
+}));
+
+describe('useDashboard hook', () => {
+	test('Should get data from redux store', () => {
+		const { result } = renderHook(() => useCharacters());
+
+		waitFor(() =>
+			expect(result.current.characters.length).toBe(
+				storeRedux.characters.length
+			)
+		);
+	});
+});
